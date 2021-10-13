45402cb7c01bd78f68cdcecdd2bdbc73bb224a87
loshakovaalina <92411576+loshakovaalina@users.noreply.github.com>
2021-10-13 09:09:22 +0800

    Add files via upload

---
+++ KROK.java
@@ -0,0 +1,20 @@
+import java.lang.System;
+
+public class KROK{
+
+    public static void main(String[] args){
+        for (int i = 0; i < 100; i++){
+            if (i % 3 == 0) {
+                if (i % 5 == 0)
+                    System.out.println("FizzBuzz");
+                else
+                    System.out.println("Fizz");
+            }
+            else
+            if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
+    }
+}
